1cd280d5c2e0138d16c684ee3e6e40bceb321231
root <root@oswks.semiprodint.torontomu.ca>
2023-08-12 12:11:25 -0400

    change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Line added at May 15 \n";
-print "Line added at May 15 for Web \n";
+print "Hello, World! php version is " . PHP_VERSION . "<br>";
+
+print "From WebServer1 <br>";
 
 ?>
