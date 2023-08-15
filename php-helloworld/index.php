93e94136f4e95a7cd6b80cfd2f1d604a300cb294
root <root@oswks.semiprodint.torontomu.ca>
2023-08-15 16:02:36 -0400

    Changes

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,7 +1,11 @@
 <?php
-print "Hello, World! <br>";
+date_default_timezone_set('America/New_York');
+print '<p style="font-size: 20px;">Hello World</p>';
 print "<br>";
-print "php version is " . PHP_VERSION . "<br>";
+$timestamp = time();
+$date_time = date("d-m-Y (D) H:i:s", $timestamp);
+echo "Current date and local time on this server is $date_time";
+# print "php version is " . PHP_VERSION . "<br>";
 print "<br>";
 print '<p style="font-size: 20px;">From Web2</p>';
 
