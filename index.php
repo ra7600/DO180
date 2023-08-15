2819886e9a3bfe6f3951c3391ce841adeeb9ce7a
root <root@oswks.semiprodint.torontomu.ca>
2023-08-15 14:57:42 -0400

    changes

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-print "Hello, World! <br>";
+print '<p style="font-size: 20px;">Hello World</p>';
+# print "Hello, World! <br>";
 print "<br>";
-print "php version is " . PHP_VERSION . "<br>";
+$timestamp = time();
+$date_time = date("d-m-Y (D) H:i:s", $timestamp);
+echo "Current date and local time on this server is $date_time";
+# print "php version is " . PHP_VERSION . "<br>";
 print "<br>";
 print '<p style="font-size: 20px;">From Web1</p>';
 
